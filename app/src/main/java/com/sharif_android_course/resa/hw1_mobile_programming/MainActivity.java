8d7b7e7c6eb516da84892b04011f8a79d637c95b
Saleh <salehsagharchi@gmail.com>
2020-04-10 03:35:18 +0430

    complete search box functionality

--- app/src/main/java/com/sharif_android_course/resa/hw1_mobile_programming/MainActivity.java
+++ app/src/main/java/com/sharif_android_course/resa/hw1_mobile_programming/MainActivity.java
@@ -9,10 +9,16 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 
+import com.google.android.material.textfield.TextInputLayout;
 import com.sharif_android_course.resa.hw1_mobile_programming.controllers.CitiesAdapter;
 import com.sharif_android_course.resa.hw1_mobile_programming.models.City;
 import com.sharif_android_course.resa.hw1_mobile_programming.models.CitySearchResult;
@@ -22,6 +28,13 @@ import com.sharif_android_course.resa.hw1_mobile_programming.workers.ThreadManag
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +44,17 @@ public class MainActivity extends AppCompatActivity {
     private ThreadManager threadManager;
     private List<City> cityList;
     private CitiesAdapter citiesAdapter;
+    private ScheduledExecutorService searchExecutor = null;
+    private ScheduledFuture scheduledSeacrh = null;
+    private TextInputLayout citySearch;
+    private ProgressBar prg;
+    private RecyclerView rvCities;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,13 +74,37 @@ public class MainActivity extends AppCompatActivity {
 
         printThreadInfo("main");
 
-
-        RecyclerView rvCities = findViewById(R.id.rvCities);
+        rvCities = findViewById(R.id.rvCities);
         cityList = new ArrayList<>();
         citiesAdapter = new CitiesAdapter(cityList);
         rvCities.setAdapter(citiesAdapter);
         rvCities.setLayoutManager(new LinearLayoutManager(this));
 
+        prg = findViewById(R.id.loading);
+        setLoading(false);
+
+        citySearch = findViewById(R.id.citySearchLayout);
+        Objects.requireNonNull(citySearch.getEditText()).addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                MainActivity.this.scheduleSearching(s.toString());
+                if (s.toString().equals("")) {
+                    setLoading(false);
+                } else {
+                    setLoading(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
 
     }
@@ -72,6 +120,7 @@ public class MainActivity extends AppCompatActivity {
                     cityList.add(c);
                     citiesAdapter.notifyItemInserted(cityList.size() - 1);
                 }
+                setLoading(false);
             } else if (msg.obj instanceof String) {
                 if (((String) msg.obj).startsWith("Error")) {
                     this.showErrorToUser((String) msg.obj);
@@ -85,7 +134,32 @@ public class MainActivity extends AppCompatActivity {
                     this.showErrorToUser((String) msg.obj);
                 }
             }
+        } else if (msg.obj.equals("StartSearch")) {
+            String city = Objects.requireNonNull(citySearch.getEditText()).getText().toString();
+            if (!city.equals("")) {
+                threadManager.ExecuteCityRequest(city, getString(R.string.city_token));
+            }
         }
+
+    }
+
+
+    public void scheduleSearching(String cityName) {
+        if (searchExecutor == null)
+            searchExecutor = Executors.newSingleThreadScheduledExecutor();
+
+        if (this.scheduledSeacrh != null) {
+            if (!this.scheduledSeacrh.isDone())
+                this.scheduledSeacrh.cancel(false);
+        }
+
+        Runnable task = () -> {
+            Message msg = new Message();
+            msg.obj = "StartSearch";
+            getCityHandler().sendMessage(msg);
+        };
+
+        this.scheduledSeacrh = searchExecutor.schedule(task, 200, TimeUnit.MILLISECONDS);
     }
 
     public void showErrorToUser(String text) {
@@ -95,6 +169,16 @@ public class MainActivity extends AppCompatActivity {
 
     public Handler getCityHandler() {
         return cityHandler;
+    }
+
+    private void setLoading(boolean loading) {
+        if (loading) {
+            prg.setVisibility(View.VISIBLE);
+            rvCities.setVisibility(View.INVISIBLE);
+        } else {
+            prg.setVisibility(View.INVISIBLE);
+            rvCities.setVisibility(View.VISIBLE);
+        }
     }
 
     public static void printThreadInfo(String text) {
