08f67afec6247104aa9942ef9fad2a9fcce44f18
Saleh <salehsagharchi@gmail.com>
2020-04-09 14:19:28 +0430

    initial_commit

---
+++ app/src/main/java/com/sharif_android_course/resa/hw1_mobile_programming/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sharif_android_course.resa.hw1_mobile_programming;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
