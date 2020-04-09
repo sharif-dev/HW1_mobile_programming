f73d82504d040b6812a3cb8b9298131b66660f94
Saleh <salehsagharchi@gmail.com>
2020-04-09 15:21:08 +0430

    test good color for app

--- app/src/main/java/com/sharif_android_course/resa/hw1_mobile_programming/MainActivity.java
+++ app/src/main/java/com/sharif_android_course/resa/hw1_mobile_programming/MainActivity.java
@@ -3,12 +3,16 @@ package com.sharif_android_course.resa.hw1_mobile_programming;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //test comment
+        Log.i("test","salam");
+
         setContentView(R.layout.activity_main);
     }
 }
